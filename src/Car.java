0998b28204f2a4d2c11d88139e34d7351bb23d1a
singtopza <59428798+singtopza@users.noreply.github.com>
2021-07-19 15:41:50 +0700

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+    private String colour;
+
+    private int maxi_Speed;
+
+    public void carDetails(){
+
+        System.out.println("Car Colour= "+colour + "; Maximum Speed= " + maxi_Speed);
+
+    }
+
+    //Setting colour of the car
+
+    public void setColour(String colour) {
+
+        this.colour = colour;
+
+    }
+
+    //Setting maximum car Speed
+
+    public void setMaxSpeed(int maxi_Speed) {
+
+        this.maxi_Speed = maxi_Speed;
+
+    }
+}
---
+++ src/CarEngine.java
@@ -0,0 +1,13 @@
+public class CarEngine {
+    public void startEngine(){
+
+        System.out.println("The car engine has Started.");
+
+    }
+
+    public void stopEngine(){
+
+        System.out.println("The car engine has Stopped.");
+
+    }
+}
